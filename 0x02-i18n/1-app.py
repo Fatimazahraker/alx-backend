6e0e748600f07c4a8a761d3260336374f59008aa
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2024-02-06 21:54:32 +0000

    task

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,8 +2,20 @@
 """Basic Flask app"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
